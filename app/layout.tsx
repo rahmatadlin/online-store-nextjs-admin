7a7d9518ed7ad7b56ac0783f3e4b5e40333c5925
rahmatadlin <rahmatadlin210@gmail.com>
2024-08-02 01:21:48 +0700

    modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,29 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
-
-// import { ToasterProvider } from "@/providers/toast-provider";
-// import { ModalProvider } from "@/providers/modal-provider";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Admin",
-  description: "Admin",
-};
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import './globals.css'
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
-          {/* <ToasterProvider />
-          <ModalProvider /> */}
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
           {children}
         </body>
       </html>
     </ClerkProvider>
-  );
+  )
 }
